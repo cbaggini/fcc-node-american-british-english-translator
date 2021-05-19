3bd4ca256287fefeab18be787d3522193ee1c78e
Cecilia <earwen1986@gmail.com>
2021-05-19 19:28:14 +0100

    Initial version of translator

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,55 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
+//missing: times and spans
 class Translator {
+  getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+  translateToBritish(originalText) {
+    let translatedText = originalText;
+    Object.keys(americanOnly).forEach((el) => {
+      if (translatedText.includes(el)) {
+        translatedText.replace(el, americanOnly[el]);
+      }
+    });
+    Object.keys(americanToBritishTitles).forEach((el) => {
+      if (translatedText.includes(el + " ")) {
+        translatedText.replace(el, americanToBritishTitles[el]);
+      }
+    });
+    Object.keys(americanToBritishSpelling).forEach((el) => {
+      if (translatedText.includes(el)) {
+        translatedText.replace(el, americanToBritishSpelling[el]);
+      }
+    });
 
+    return translatedText;
+  }
+  translateToAmerican(originalText) {
+    let translatedText = originalText;
+    Object.keys(britishOnly).forEach((el) => {
+      if (translatedText.includes(el)) {
+        translatedText.replace(el, britishOnly[el]);
+      }
+    });
+    Object.values(americanToBritishTitles).forEach((el) => {
+      if (translatedText.includes(el + " ")) {
+        let replacement = this.getKeyByValue(americanToBritishTitles, el);
+        translatedText.replace(el, replacement);
+      }
+    });
+    Object.values(americanToBritishSpelling).forEach((el) => {
+      if (translatedText.includes(el)) {
+        let replacement = this.getKeyByValue(americanToBritishSpelling, el);
+        translatedText.replace(el, replacement);
+      }
+    });
+
+    return translatedText;
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,25 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const originalText = req.body.text;
+    const locale = req.body.locale;
+    if (originalText === "") {
+      res.json({ error: "No text to translate" });
+    } else if (
+      originalText &&
+      (locale === "american-to-british" || locale === "british-to-american")
+    ) {
+      let translatedText;
+      res.json({ text: originalText, translation: translatedText });
+    } else if (originalText && locale) {
+      res.json({ error: "Invalid value for locale field" });
+    } else {
+      res.json({ error: "Required field(s) missing" });
+    }
+  });
 };
